5b28397afd826b41c6bbc515e0f8b0c7b6364080
Quisoor <bretinaurelien@gmail.com>
2018-11-19 23:28:11 +0100

    Pagination test part

---
+++ src/FluentApiNet.Test/Tools/PaginationToolsTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentApiNet.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentApiNet.Test.Tools
+{
+    [TestClass]
+    public class PaginationToolsTest
+    {
+        [TestMethod]
+        public void LimitPage_Null()
+        {
+            var actual = PaginationTools.LimitPage(null);
+            var expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LimitPage_Zero()
+        {
+            var actual = PaginationTools.LimitPage(0);
+            var expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LimitPageSize_Null()
+        {
+            var actual = PaginationTools.LimitPageSize(null);
+            var expected = 25;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LimitPageSize_InferiorToZero()
+        {
+            var actual = PaginationTools.LimitPageSize(-1);
+            var expected = 25;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

018a5bd55bc56fd0f40a1d670a8ecd58675b52c4
Quisoor <bretinaurelien@gmail.com>
2018-11-19 23:11:30 +0100

    Add transpose funcionnality

--- src/FluentApiNet.Core/ServiceBase.cs
+++ src/FluentApiNet.Core/ServiceBase.cs
@@ -13,6 +13,16 @@ namespace FluentApiNet.Core
         where TContext : DbContext
     {
         /// <summary>
+        /// The default page
+        /// </summary>
+        private const int DEFAULT_PAGE = 1;
+
+        /// <summary>
+        /// The default pagesize
+        /// </summary>
+        private const int DEFAULT_PAGESIZE = 25;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="ServiceBase{TModel, TEntity, TContext}"/> class.
         /// </summary>
         public ServiceBase()
@@ -43,7 +53,18 @@ namespace FluentApiNet.Core
         /// <returns></returns>
         public Results<TModel> Get(Expression<Func<TModel, bool>> filters)
         {
-            return this.Get(filters, 1, 25);
+            return this.Get(filters, DEFAULT_PAGE, DEFAULT_PAGESIZE);
+        }
+
+        /// <summary>
+        /// Gets the specified filters.
+        /// </summary>
+        /// <param name="filters">The filters.</param>
+        /// <param name="page">The page.</param>
+        /// <returns></returns>
+        public Results<TModel> Get(Expression<Func<TModel, bool>> filters, int? page)
+        {
+            return this.Get(filters, page, DEFAULT_PAGESIZE);
         }
 
         /// <summary>
@@ -57,6 +78,21 @@ namespace FluentApiNet.Core
         {
             var results = new Results<TModel>();
 
+            // format pagination
+            page = LimitPage(page);
+            pageSize = LimitPageSize(pageSize);
+
+            var query = GetQuery(filters);
+
+            // get the count
+            results.Count = query.Count();
+
+            // apply pagination
+            query = query.Skip(page.Value - 1 * pageSize.Value).Take(pageSize.Value);
+
+            // get the results
+            results.Result = Transpose(query);
+
             return results;
         }
 
@@ -67,7 +103,9 @@ namespace FluentApiNet.Core
         /// <returns></returns>
         protected IQueryable<TEntity> GetQuery(Expression<Func<TModel, bool>> filters)
         {
+            var translator = new TranslationVisitor(SelectMapping);
             var query = GetQuery();
+            query = query.Where(Expression.Lambda<Func<TEntity, bool>>(translator.Visit(filters)));
             return query;
         }
 
@@ -82,16 +120,54 @@ namespace FluentApiNet.Core
             return repository;
         }
 
-        /// <summary>
-        /// Transposes the filter.
-        /// </summary>
-        /// <param name="filters">The filters.</param>
-        /// <returns></returns>
-        private Expression<Func<TEntity, bool>> TransposeFilter(Expression<Func<TModel, bool>> filters)
+        private List<TModel> Transpose(IQueryable<TEntity> query)
         {
             var parameter = Expression.Parameter(typeof(TEntity));
-            filters.
-            return null;
+            var ctor = Expression.New(typeof(TModel));
+            var assignments = new List<MemberAssignment>();
+            foreach(var map in SelectMapping)
+            {
+                var property = typeof(TModel).GetProperty(map.ModelMember.Member.Name);
+                assignments.Add(Expression.Bind(property, map.EntityMember));
+
+            }
+            var init = Expression.MemberInit(ctor, assignments.ToArray());
+            var select = Expression.Lambda<Func<TEntity, TModel>>(init, parameter);
+            return query.Select(select).ToList();
+        }
+
+        /// <summary>
+        /// Limits the page.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <returns></returns>
+        private int LimitPage(int? page)
+        {
+            if (page.HasValue && page.Value > 0)
+            {
+                return page.Value;
+            }
+            else
+            {
+                return DEFAULT_PAGE;
+            }
+        }
+
+        /// <summary>
+        /// Limits the size of the page.
+        /// </summary>
+        /// <param name="pageSize">Size of the page.</param>
+        /// <returns></returns>
+        private int LimitPageSize(int? pageSize)
+        {
+            if (pageSize.HasValue && pageSize.Value >= 0)
+            {
+                return pageSize.Value;
+            }
+            else
+            {
+                return DEFAULT_PAGESIZE;
+            }
         }
     }
 }

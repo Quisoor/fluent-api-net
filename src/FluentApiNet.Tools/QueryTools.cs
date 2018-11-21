63494c197f1bd530d86a0c0c797ed5d0e11830fc
Quisoor <bretinaurelien@gmail.com>
2018-11-21 20:37:37 +0100

    Code review of tools part

--- src/FluentApiNet.Tools/QueryTools.cs
+++ src/FluentApiNet.Tools/QueryTools.cs
@@ -18,17 +18,24 @@ namespace FluentApiNet.Tools
         /// <returns>List of model</returns>
         public static List<TModel> Transpose<TModel, TEntity>(IQueryable<TEntity> query, List<Mapping> mappings)
         {
+            // define principal parameter
             var parameter = Expression.Parameter(typeof(TEntity), "x");
-            var traductor = new TranslationVisitor<TEntity>(mappings, parameter);
+            // initialize translator
+            var translator = new TranslationVisitor<TEntity>(mappings, parameter);
+            // new TModel()
             var ctor = Expression.New(typeof(TModel));
             var assignments = new List<MemberAssignment>();
             foreach (var map in mappings)
             {
-                var property = typeof(TModel).GetProperty(map.ModelMember.Member.Name);                
-                assignments.Add(Expression.Bind(property, traductor.Visit(map.ModelMember)));
+                // add assignment for the model property
+                var property = typeof(TModel).GetProperty(map.ModelMember.Member.Name);
+                assignments.Add(Expression.Bind(property, translator.Visit(map.ModelMember)));
             }
+            // initialize the model
             var init = Expression.MemberInit(ctor, assignments.ToArray());
+            // create select expression
             var select = Expression.Lambda<Func<TEntity, TModel>>(init, parameter);
+            // apply select expression
             return query.Select(select).ToList();
         }
     }

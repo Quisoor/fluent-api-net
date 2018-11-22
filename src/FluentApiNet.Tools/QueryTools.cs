ac4179bd5f38213a4a27ed3d1b81b1a213725a9d
Quisoor <bretinaurelien@gmail.com>
2018-11-22 13:59:03 +0100

    Add comment

--- src/FluentApiNet.Tools/QueryTools.cs
+++ src/FluentApiNet.Tools/QueryTools.cs
@@ -18,7 +18,7 @@ namespace FluentApiNet.Tools
         /// <param name="query">The query.</param>
         /// <param name="page">The page.</param>
         /// <param name="pageSize">Size of the page.</param>
-        /// <returns></returns>
+        /// <returns>Pagined query</returns>
         public static IQueryable<TEntity> Pagine<TEntity>(this IQueryable<TEntity> query, int page, int pageSize)
         {
             var skip = (page - 1) * pageSize;

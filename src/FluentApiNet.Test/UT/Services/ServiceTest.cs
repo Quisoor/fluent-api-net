f015f5396d005cc07293415e0d1c19c397a30385
Quisoor <quisoor@outlook.com>
2019-04-09 22:00:29 +0200

    Add get all to aggregation service

--- src/FluentApiNet.Test/UT/Services/ServiceTest.cs
+++ src/FluentApiNet.Test/UT/Services/ServiceTest.cs
@@ -165,6 +165,25 @@ namespace FluentApiNet.Test.UT.Services
             Assert.IsNotNull(result);
         }
 
+        [TestMethod]
+        public void ServiceTest_Aggregation_GetAll()
+        {
+            var connection = Effort.DbConnectionFactory.CreateTransient();
+            var context = new TestDbContext(connection);
+            context.Users.Add(new User { Id = 1, Name = "NAME1", Role = new Role { Id = 1, Name = "ROLE1" } });
+            context.Users.Add(new User { Id = 2, Name = "NAME2", Role = new Role { Id = 2, Name = "ROLE2" } });
+            context.Users.Add(new User { Id = 3, Name = "NAME3", Role = new Role { Id = 3, Name = "ROLE3" } });
+            context.Locations.Add(new Location { Id = 1, Name = "PARIS", UserId = 1 });
+            context.Locations.Add(new Location { Id = 2, Name = "MADRID", UserId = 2 });
+            context.Locations.Add(new Location { Id = 3, Name = "LONDON", UserId = 1 });
+            context.SaveChanges();
+            var userService = new UserService(context);
+            var locationService = new LocationService(context);
+            var service = new AggregationService(locationService, userService);
+            var result = service.Get();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+        }
 
         [TestMethod]
         public void ServiceTest_Aggregation_Get_WithoutCondition()
